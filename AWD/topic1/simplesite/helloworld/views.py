9e37d206b6ca3e53850951c0d5f60de8a701d615
Rebecca Heyman <rebecca@heyman.com>
2026-04-27 12:25:47 -0500

    additional view on simplesite and add to instructions

--- AWD/topic1/simplesite/helloworld/views.py
+++ AWD/topic1/simplesite/helloworld/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from .models import *
 
 def index(request):
   response_string = Hello.objects.all()[0]
   return render(request, 'helloworld/index.html', {'data': response_string})
+
+def simple_view(request):
+  header = request.META
+  ip = header['REMOTE_ADDR']
+  html = "<html><body><h1>Your IP Address is: " + ip + "</h1></body></html>"
+  return HttpResponse(html, content_type="text/html", status=200)
