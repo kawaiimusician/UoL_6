abcd421b615f25b073512cbafbcc91a63e4486e3
Rebecca Heyman <rebecca@heyman.com>
2026-05-11 17:59:15 -0500

    more gene functionality

---
+++ AWD/topic2/bioweb/genedata/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('', views.index, name='index'),
+  path('gene/<int:pk>/', views.gene, name='gene'),
+  path('list/<str:type>', views.list, name='list'),
+  path('poslist/', views.poslist, name='poslist'),
+  path('delete/<int:pk>', views.delete, name='delete'),
+]
